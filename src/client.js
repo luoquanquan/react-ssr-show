9b0fb5352770139622cff9d8e04b027016ef6435
luoquanquan <luoquanquan@baidu.com>
2019-07-15 14:41:08 +0800

    配置路由
    1. 安装 react-router-dom 依赖
    2. 修改配置入口文件 client 和 App.jsx 安排上 react-router

--- src/client.js
+++ src/client.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App.jsx';
 
-// eslint-disable-next-line
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('app'),
+);
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,11 @@
 import React from 'react';
+import { NavLink, Switch, Route } from 'react-router-dom';
+
+import Home from './Home.jsx';
+import Zhuapai from './Zhuapai.jsx';
+import Kanpai from './Kanpai.jsx';
+import Xipai from './Xipai.jsx';
+import Mapai from './Mapai.jsx';
 
 export default class App extends React.Component {
   constructor(props) {
@@ -10,6 +17,19 @@ export default class App extends React.Component {
     return (
       <div className="ssr-show">
         <h1>欢迎来到澳门皇冠赌场</h1>
+        <NavLink to="/">首页</NavLink>
+        <NavLink to="/Zhuapai">抓牌</NavLink>
+        <NavLink to="/Kanpai">看牌</NavLink>
+        <NavLink to="/Xipai">洗牌</NavLink>
+        <NavLink to="/Mapai">码牌</NavLink>
+
+        <Switch>
+          <Route path="/" component={Home} />
+          <Route path="/Zhuapai" component={Zhuapai} />
+          <Route path="/Kanpai" component={Kanpai} />
+          <Route path="/Xipai" component={Xipai} />
+          <Route path="/Mapai" component={Mapai} />
+        </Switch>
       </div>
     );
   }
