524a9e0909c19f0e23e6e528408f18f5c1935587
luoquanquan <luoquanquan@baidu.com>
2019-07-27 12:21:55 +0800

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,9 @@ module.exports = {
     "rules": {
         'import/extensions': 0,
         'react/jsx-filename-extension': 0,
-        'no-underscore-dangle': 0
+        'no-underscore-dangle': 0,
+        'react/destructuring-assignment': 0,
+        'react/prop-types': 0
     },
     "env": {
         "browser": true,
