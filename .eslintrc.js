37f3781742df9428e6689dbbe61bc065c83d10f0
luoquanquan <luoquanquan@baidu.com>
2019-07-27 10:47:32 +0800

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
     "extends": "airbnb",
     "rules": {
         'import/extensions': 0,
-        'react/jsx-filename-extension': 0
+        'react/jsx-filename-extension': 0,
+        'no-underscore-dangle': 0
     },
     "env": {
         "browser": true,
