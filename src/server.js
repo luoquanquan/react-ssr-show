73bc58850ca9bc2407164e8bf3614f7282a8e5fa
luoquanquan <luoquanquan@baidu.com>
2019-08-16 16:40:21 +0800

    这个, 体会...

--- src/server.js
+++ src/server.js
@@ -3,11 +3,12 @@ import Koa from 'koa';
 import Router from 'koa-router';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
-import { StaticRouter } from 'react-router-dom';
+import { StaticRouter, matchPath } from 'react-router-dom';
 import serve from 'koa-static';
 import { Provider } from 'react-redux';
 import App from './components/App.jsx';
 import createStore, { init } from './store';
+import routes from './router';
 
 const app = new Koa();
 const router = new Router();
@@ -34,7 +35,7 @@ const generateHtmlStr = (reactDom, reduxState) => `
 </html>
 `;
 
-router.get('*', (ctx) => {
+router.get('*', async (ctx) => {
   const context = {};
   const { url } = ctx.req;
   const store = createStore();
@@ -43,6 +44,19 @@ router.get('*', (ctx) => {
 
   // 获取首屏情况下的状态
   const reduxState = store.getState();
+
+  // 初始化时是否需要访问异步接口
+  // 首先找到当前路由, 对应的组件
+  const dataRequireMents = routes.filter(page => matchPath(ctx.req.url, page))
+    // 把路由匹配的结果对应的组件拿出来
+    .map(page => page.component)
+    // 把定义了 serverFetch 静态方法的组件过滤出来
+    .filter(comp => comp.serverFetch)
+    // 嗯哼...
+    .map(comp => store.dispatch(comp.serverFetch()));
+
+  // 等...
+  if (dataRequireMents.length) await Promise.all(dataRequireMents);
 
   // 首先把 React 组件变成一个字符串
   // eslint-disable-next-line
