5aa99a3d6710756e3471cf35d3b425afaac592cd
luoquanquan <luoquanquan@baidu.com>
2019-08-16 14:40:38 +0800

    更新看牌页面, 一边听歌一边看牌

--- src/components/Kanpai.jsx
+++ src/components/Kanpai.jsx
@@ -1,5 +1,43 @@
 import React from 'react';
 
-export default () => (
-  <h2>看牌页面</h2>
-);
+import { connect } from 'react-redux';
+import { fetchData } from '../store';
+
+class Kanpai extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    const { songList, fetchData: fetchListData } = this.props;
+    if (!songList.length) {
+      fetchListData();
+    }
+  }
+
+  render() {
+    const { songList } = this.props;
+    console.log(`当前时间 ${Date.now()}: debug 的数据是 this.props: `, this.props);
+    return (
+      <div>
+        <h3>this page is the Home</h3>
+        {
+          songList.map(({ id, title, picUrl }) => (
+            <dl key={id}>
+              <dt><img src={picUrl} alt="" srcSet="" /></dt>
+              <dd>{title}</dd>
+            </dl>
+          ))
+        }
+
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = { fetchData };
+
+const mapStateToProps = ({ songReducer: songList }) => ({ songList });
+
+export default connect(mapStateToProps, mapDispatchToProps)(Kanpai);
