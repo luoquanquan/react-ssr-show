2a78d38e52e5d8267747370717002ad0d457cb22
luoquanquan <luoquanquan@baidu.com>
2019-08-16 11:47:17 +0800

    升级 store.js 实现多 reducer

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux';
+import getMockData from './mock';
 
 // actionTypes
 // session
@@ -6,11 +7,21 @@ const INIT = 'INIT';
 const LOGIN = 'LOGIN';
 const LOGOUT = 'LOGOUT';
 
+// song
+const INITSONG = 'INITSONG';
+
 
 // actionsCreaters
 export const init = () => ({ type: INIT });
 export const logIn = () => ({ type: LOGIN });
 export const logOut = () => ({ type: LOGOUT });
+
+export const initSongList = list => ({ type: INITSONG, list });
+
+// 把歌曲列表添加到 store 里边
+const storeData = list => ({ type: INITSONG, list });
+
+export const fetchData = () => dispatch => getMockData().then(data => dispatch(storeData(data)));
 
 // reducers
 const sessionReducer = (state = false, action) => {
@@ -27,7 +38,20 @@ const sessionReducer = (state = false, action) => {
   }
 };
 
-export default initState => createStore(
+const songReducer = (state = [], action) => {
+  switch (action.type) {
+    case INITSONG:
+      return action.list;
+    default: return state;
+  }
+};
+
+const reducer = combineReducers({
   sessionReducer,
+  songReducer,
+});
+
+export default initState => createStore(
+  reducer,
   initState,
 );
